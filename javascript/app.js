aa1f254c917de5c2641c732d14aea6325a9d532c
Justin Klass <Justin@Justins-MacBook.local>
2019-10-15 12:28:43 -0400

    Added button with jquery that grabs the sprite and puts it on the page

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,20 @@
 $(() => {
-    console.log('Hello World');
+    const $container = $('#team1');
+    $('#create').on('click', (event) => {
+        for(i = 0; i < 1; i++) {
+            let pokemon = Math.floor(Math.random() * Math.floor(808));
+            console.log(pokemon);
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/`,
+                type: "GET",
+            }).done(function(data) {
+                console.log(data);
+                console.log(data.sprites);
+                console.log(data.sprites.front_default);
+                const $poke_sprite = $(`<img src=${data.sprites.front_default}>`);
+                $container.append($poke_sprite);
+
+        })
+        }
+    })
 })
