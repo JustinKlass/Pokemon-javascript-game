cfc4f35ca13e2a32dbd536acd89ca247a5294e64
Justin Klass <Justin@Justins-MacBook.home>
2019-10-15 21:20:41 -0400

    Broke down main javascript into more readable functions

--- javascript/app.js
+++ javascript/app.js
@@ -3,89 +3,51 @@ class getPokemonStats {
         this.name = name;
         this.hp = hp;
         this.sprite = sprite;
-        // this.gender = gender;
+        this.gender = gender;
         this.attack1 = '';
         this.attack2 = '';
         this.attack3 = '';
         this.attack4 = '';
     }
 }
-
-// $(() => {
-
-// const makePokemon = () => {
-//         let randPokemon = Math.floor(Math.random() * Math.floor(803));
-//         $.ajax({
-//             url: `https://pokeapi.co/api/v2/pokemon/${randPokemon}/`,
-//             type: "GET",
-//         }).done(function(data) {
-//             // let $gender;
-//             // if (Math.random() < 0.5) {
-//             //     $gender = 'img/female.png';
-//             // }
-//             // else {
-//             //     $gender = 'img/male.png';
-//             // }
-//             //console.log(new pokemon(data.name, data.stats[5].base_stat, data.sprites.front_default));
-//             const pokemon = new getPokemonStats(data.name, data.stats[5].base_stat, data.sprites.front_default);
-//             console.log(pokemon);
-//             console.log(data.sprites)
-//             const $container = $('#team1');
-//             const $div = $(`<div class='team'>`);
-//             const $img = $(`<img src='${pokemon.sprite.front_default}'>`);
-
-//             $container.append($div);
-//             $div.append($img);
-    
-//         });
-//     }
-//     // const appendTeam = () => {
-//     //     makePokemon();
-//     //     const $container = $('#team1');
-//     //     const $poke_div = $(`<div id=${i} class='team'>`);
-//     //     const $poke_sprite = $(`<img src=${data.sprite} class='sprite'>`);
-//     //     // const $poke_gender = $(`<img src='${$gender}' class='gender'>`);
-//     //     const $poke_name = $("<h3 class='title'>");
-//     //     $poke_name.text($poki.name);
-    
-//     //     $container.append($poke_div);
-//     //     $poke_div.append($poke_sprite);
-//     //     $poke_div.append($poke_name);
-//     //     // $poke_div.append($poke_gender);
-    
-//     //     console.log(data);
-//     // }
-
-// const loop = (num) => {
-//     for(i = 0; i < num; i++) {
-//         makePokemon();
-//     }
-// }
-//     $('#create').on('click', (event) => {
-//         loop(2);
-//     })
-// })
-
-const makePokemon = (randPokemon) => {
+const getRandomGender = () => {
+    if( Math.random() < 0.5) {
+        return 'img/female.png';
+    }
+    else {
+        return 'img/male.png';
+    }
+}
+const getRandomId = () => {
+    let randPokemon = Math.floor(Math.random() * Math.floor(803));
+    return randPokemon;
+}
+const makePokemon = (randomId) => {
     const $container = $('#team1');
+
     $.ajax({
-        url: `https://pokeapi.co/api/v2/pokemon/${randPokemon}/`,
+        url: `https://pokeapi.co/api/v2/pokemon/${randomId}/`,
         type: "GET",
     }).done(function(data) {
-        console.log(data);
-        console.log(data.sprites);
-        console.log(data.sprites.front_default);
-        const $poke_sprite = $(`<img src=${data.sprites.front_default}>`);
-        $container.append($poke_sprite);
+        const team = $("<div class = 'pokemonBox'>"); 
+        const sprite = $(`<img src = ${data.sprites.front_default} class = 'sprite'>`);
+        const name = $("<p class = 'pokemonName'>");
+        const gender = $(`<img src = '${getRandomGender()}' class = 'gender'>`);
+        name.text(data.name);
+
+        $container.append(team);
+        team.append(sprite);
+        team.append(name);
+        team.append(gender);
 
     })
 }
 
 $(() => {
     $('#create').on('click', (event) => {
-        for(i = 0; i < 4; i++) {
-            let randPokemon = Math.floor(Math.random() * Math.floor(808));
-            makePokemon(randPokemon);
+        for(i = 0; i < 2; i++) {
+            const randomId = getRandomId();
+            makePokemon(randomId);
         }
     })
 })
